f37a6b16619a9a015b7f677028a6f98b7534ca33
Z. D. Smith <cruxdestruct@gmail.com>
2014-02-20 16:12:40 -0500

    Working GET and POST'

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 from fleshl import Fleshl
 from wsgiref.simple_server import make_server
-from cgi import parse_qs, escape
+
 
 app = Fleshl()
 
 @app.add_route('/')
 def index():
-	return "There's nothing here!"
+	return ("You asked for %s." % app.request.query, 'text/plain')
 	
+@app.add_route('/form')
+def form():
+	html = """
+	<html>
+	<body>
+	<form method="post" action="/form">
+		<p>
+			<input type="text" name="arg">
+			<input type="submit" value="Submit">
+		</p>
+	</form>
+	<p>
+	You entered %s.
+	</p>
+	</html>
+	"""
+	
+	if 'arg' not in app.request.post_data:
+		readback = "Give some data!"
+	elif app.request.post_data['arg']:
+		readback = app.request.post_data['arg'][0]
+	else:
+		readback = "No information"
+	return (html % readback, 'text/html')
+
+
 httpd = make_server('localhost', 5000, app.run)
 httpd.serve_forever()
--- fleshl.py
+++ fleshl.py
@@ -1,26 +1,69 @@
+from urlparse import parse_qs
+import re
+
+class Request(object):
+	def __init__(self, environ):
+		def pull_environ(key):
+			try:
+				return environ[key]
+			except KeyError:
+				return None
+
+		self.query = pull_environ('PATH_INFO')
+		self.method = pull_environ('REQUEST_METHOD').upper()
+		content_length = pull_environ('CONTENT_LENGTH')
+		if content_length:
+			self.length = int(content_length)
+
+		self.post_data = {}
+		self.q_strings = {}
+
+		if self.method is 'GET' and '?' in self.query:
+			self.q_strings = self.query[self.query.index('?'):-1]
+
+		if 'POST' in self.method and self.length:
+			print "assigning post data to request"
+			wsgi_input = pull_environ('wsgi.input').read(self.length)
+			print "pulled out wsgi_input"
+			self.post_data = parse_qs(wsgi_input)
+
+	def __repr__(self):
+		return "<" + self.query + self.method + str(self.length) + \
+				", ".join(self.post_data.keys()) + ">"
+
 class Fleshl(object):
-    def __init__(self):
-        self.routes = {}
+	def __init__(self):
+		self.routes = {}
 
-    def add_route(self, route):
-        def wrapped(func):
-	        self.routes[route] = func
-        return wrapped
+	def add_route(self, route):
+		def wrapped(func):
+			self.routes[route] = func
+		return wrapped
 
-    def run(self, environ, start_response):
+	def render(self, template, values):
+		tag_search = re.compile("{{ (?P=<tag>) }}")
+		'''
+		>>> tag_search.search("Lorem \{\{ Ipsum \}\}")
+		'''
+		
+
+	def run(self, environ, start_response):
 		# Run the function associated with the URL we pull from
 		# environ
 		
-		# import pdb
-		# pdb.set_trace()
-		response_body = self.routes[environ.get('PATH_INFO')]()
+		import doctest
+		doctest.testmod()
 		
-		if response_body:
+		self.request = Request(environ)
+		response = self.routes[environ.get('PATH_INFO')]()
+
+		if response:
 			status = '200 OK'
 		else:
 			status = '404 NOT FOUND'
-		response_headers = [('Content-Type', 'text/plain'), 
-		('Content-Length', str(len(response_body)))] 
-		
+		response_headers = [('Content-Type', response[1]), 
+		('Content-Length', str(len(response[0])))] 
+
 		start_response(status, response_headers)
-		return response_body
+		return response[0]
+
