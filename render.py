a0d2f7d4c18617443d833642cf1760506ed4a28e
Z. D. Smith <cruxdestruct@gmail.com>
2014-02-24 14:38:27 -0500

    ..

--- render.py
+++ render.py
@@ -6,23 +6,21 @@ def render(template, app):
 
     text = ""
     filename = "templates/" + template
-    extend_search = re.compile(r"{{%(extends) (.*)%}}")
+    extend_search = re.compile(r"{{(extends) (.*)}}")
     if_search = re.compile("{{if (?P<condition>\w+)}}(?P<content>.*){{endif}}", re.DOTALL)
     replace_search = re.compile("{{ (?P<replace>) }}")
     with open(filename) as f:
         text = f.read()
-        
         # First compile template into extended base template.
         is_child = re.search(extend_search, text.splitlines()[0])
         if is_child:
             base_filename = "templates/" + is_child.groups()[1]
             with open(base_filename) as base:
                 text = extend_template(base.read(), text)
-
+        # Run conditional checks
         has_conditions = re.search(if_search, text)
         if has_conditions:
             text = render_conditionals(if_search, text, app)
-
         # Replace any variables passed to the render function.
         for replace in app.context.replaces.keys():
             arg_search = re.compile("{{ " + replace + " }}")
